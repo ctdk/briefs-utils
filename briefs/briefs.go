63d8da3c53d19eca4cb4923f338ab9bacada244b
ctdk <jbingham@gmail.com>
2026-07-03 10:50:15 -0700

    bump the patch version in light of the many changes recently, but not the minor since none of them affected compatibility

--- briefs/briefs.go
+++ briefs/briefs.go
@@ -116,7 +116,7 @@ const (
 	// BrieFS version numbers for this version of briefs-utils
 	BrieFSMajorVersion = 0
 	BrieFSMinorVersion = 9
-	BrieFSPatchVersion = 3
+	BrieFSPatchVersion = 4
 
 	// feature_incompat bits (superblock FeatIncompat field). A clean break gates
 	// the on-disk format: mkfs sets the bits for the features it writes;
